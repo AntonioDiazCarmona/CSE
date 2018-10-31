8f0469baa681e0b3e93a40d7721cf643566ed961
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2018-10-31 08:45:14 -0700

    BasicPythonNotes

--- notes/Basic Python Notes.py
+++ notes/Basic Python Notes.py
@@ -74,11 +74,37 @@ for i in range(20):
          f(i)
 
 for i in range(5):
-        print(i**2)
+     print(i**2)
 
 # while loops
 a = 0
 while a < 10:
     print(a)
     a += 1 # This is the same things as a = a +1
-    
+'''
+Hint with loops 
+For loops - Use when you know EXACTLY how many  iterations 
+While loops - Use when you DON'T know how many iterations
+'''
+
+
+# Random numbers
+import random  # This would always be on line 1
+print(random.randint(0, 100))
+
+
+# Control Statements
+def grade_calc(percentage):
+    if percentage >= 90:
+        return"A"
+    elif percentage >= 80:
+        return"B"
+    elif percentage >= 70:
+        return"C"
+    elif percentage >= 60:
+        return"D"
+    else:
+        return"F"
+
+
+print(grade_calc(60))
