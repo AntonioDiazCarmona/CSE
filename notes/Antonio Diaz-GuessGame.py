ad32c1ee2de275c61f9160320c05045383558e2f
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2018-11-01 08:44:05 -0700

    Guess Game part 1

---
+++ notes/Antonio Diaz-GuessGame.py
@@ -0,0 +1,15 @@
+import random
+a = random.randint(1, 10)
+guess = int(input("Enter number from 1 to 10: "))
+while a != "guess":
+    print
+    if guess < a:
+        print("to low")
+        guess = int(input("Enter number from 1 to 10: "))
+    elif guess > a:
+        print("to high")
+        guess = int(input("Enter number from 1 to 10: "))
+    else:
+        print(" correct you guessed it!")
+        break
+    print
