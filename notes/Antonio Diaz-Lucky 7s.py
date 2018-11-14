f0d0b6b202d14e3170f9a12baf1c1862c3831e59
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2018-11-14 08:40:14 -0800

    lucky 7s

--- notes/Antonio Diaz-Lucky 7s.py
+++ notes/Antonio Diaz-Lucky 7s.py
@@ -1,20 +1,21 @@
 import random
-Dice_1 = random.randint(1, 6)
-Dice_2 = random.randint(1, 6)
+
 Money = 15
 Round = 1
 playing = True
-while playing is True:
-    while Money == 15:
-        Money -= 1
+while playing and Money > 0:
+    Dice_1 = random.randint(1, 6)
+    Dice_2 = random.randint(1, 6)
+    Money -= 1
     print(Dice_1 + Dice_2)
     if Dice_1 + Dice_2 == 7:
-        print("you win,you now have ",Money+4,"dollars now.")
-        if Dice_1 + Dice_2>7:
-            print("you lost.You now have",Money,"dollars now")
-        elif Dice_1 + Dice_2<7:
-            print("you lost.you now have",Money,"dollars now")
-            Round += 1
+        Money += 4
+        print("you win,you now have ",Money,"dollars now.")
+    if Dice_1 + Dice_2>7:
+        print("you lost.You now have",Money,"dollars now")
+    elif Dice_1 + Dice_2<7:
+        print("you lost.you now have",Money,"dollars now")
+    Round += 1
     if Money == 0:
         print("Ran out of money.Better luck next time")
-    playing = False
+        playing = False
