0b049bf0dbdae659bbdf4054ef7f5550cf0cae64
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2018-11-15 08:37:43 -0800

    lucky 7s

--- notes/Antonio Diaz-Lucky 7s.py
+++ notes/Antonio Diaz-Lucky 7s.py
@@ -1,21 +1,21 @@
 import random
-
-Money = 15
-Round = 1
-playing = True
-while playing and Money > 0:
-    Dice_1 = random.randint(1, 6)
-    Dice_2 = random.randint(1, 6)
-    Money -= 1
-    print(Dice_1 + Dice_2)
-    if Dice_1 + Dice_2 == 7:
-        Money += 4
-        print("you win,you now have ",Money,"dollars now.")
-    if Dice_1 + Dice_2>7:
-        print("you lost.You now have",Money,"dollars now")
-    elif Dice_1 + Dice_2<7:
-        print("you lost.you now have",Money,"dollars now")
-    Round += 1
-    if Money == 0:
-        print("Ran out of money.Better luck next time")
-        playing = False
+roll_again = "yes"
+money = 15
+rounds = 0
+while money > 0:
+    print("rolling dice...")
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    print("The two values combined are...")
+    print(dice1 + dice2)
+    rounds += 1
+    money -= 1
+    myrole = (dice1 + dice2)
+    if myrole == 7:
+        money += 5
+        print("your roll was a 7 you earned four dollars")
+    elif myrole != 0:
+        print("sorry you did not roll a 7,roll again?")
+    elif money != 0:
+        print("sorry you ran out of money")
+print("sorry you loose. you played %d rounds" % rounds)
