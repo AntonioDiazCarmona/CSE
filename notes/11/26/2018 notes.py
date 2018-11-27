c19c1ef406e03b7750f92967b8a89ef63fcc57c6
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2018-11-27 08:46:06 -0800

    Notes

--- notes/11/26/2018 notes.py
+++ notes/11/26/2018 notes.py
@@ -28,3 +28,57 @@ print(new_list[1:4])
 print(new_list[1:])        #this goes all the way to the end no matter what and you dont have to put the number
 print(new_list[:2])
 
+holiday_list = []    #Always use square brackets to indicate a list
+holiday_list.append("tacos")
+holiday_list.append("bumblebee")
+holiday_list.append("Red dead Redemption 2")
+print(holiday_list)
+
+#notice this is object.method(parameters)"
+
+#Removing things from the a list
+holiday_list.remove("tacos")
+print(holiday_list)
+#tacos
+#Bumblebee
+#redDead Redemption 2
+"""
+1Make a new list with 3 items
+add a 4th item to the list
+3 remove one of the first three items from the list
+"""
+christmass_list = []    #Always use square brackets to indicate a list
+christmass_list.append("Black ops 4")
+christmass_list.append("Gaming pc")
+christmass_list.append("SoccerBall")                                  #Needs work
+christmass_list.append("Chicken nuggets")
+christmass_list.remove("Gaming pc")
+print(christmass_list)
+christmass_list.pop(0)      # Removes the item in index 0
+print(christmass_list)
+
+
+#tuple
+brands = ("apple", "samsung","HTC") # notice the parentheseses
+
+colors = ('blue','cyan','green','red','orange','purple','pink','teal','gold','magenta','turquoise')
+print(len(colors))
+
+# find the index
+print(colors.index("gold"))
+
+# changing things into a list
+string1 = "turquoise"
+list1 = list(string1)                          # help for the the project hang man
+print(list1)
+
+
+for character in list1:
+    if character == "u":
+        # replace with a *
+    current_index = list1.index(character)
+    list.pop(current_index)
+    list.insert(current_index, "*")
+
+#Change list into strings
+print("".join(list1))
