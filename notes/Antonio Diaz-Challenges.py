bcb7f01c05d8dff4a58bcdf0b00c544324288793
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2018-12-05 08:41:12 -0800

    challenges 3 and 4

--- notes/Antonio Diaz-Challenges.py
+++ notes/Antonio Diaz-Challenges.py
@@ -1,9 +1,26 @@
-# def challengeE1(firstname,lastname):
-#     print ("Your name is   " + lastname + " " + firstname)
-# challengeE1("John", "Doe")
+def challengeE1(firstname,lastname):
+    print ("Your name is   " + lastname + " " + firstname)
+challengeE1("John", "Doe")
+
 def challgengeE2(number):
-    number = int(input("Enter a number: "))
     if number % 2 == 0:
         print("Even number")
     else:
         print("Odd number")
+
+challgengeE2(57)
+
+
+def challengeE3(base, height):
+    area = base * height / 2
+    print("area = %d" % area)
+
+
+def challengeE4(number):
+    if number > 0:
+        print("Positive number")
+    elif number == 0:
+        print("Zero")
+    else number < 0:
+print("Negative number")
+challengeE4(56)
