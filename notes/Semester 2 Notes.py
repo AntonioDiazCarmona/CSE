4bc0f7256cb003080dc2b0d655ca7f9edb2583b3
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2019-01-08 08:44:15 -0800

    Semester 2 notes begining

---
+++ notes/Semester 2 Notes.py
@@ -0,0 +1,20 @@
+print("Hello World")
+# This is a single line comment
+cars = 5
+driving = True
+# cars = 5
+print("I have %d cars" % cars)  # How to write i have 5 cars withought typing 5 or five
+print("I have " + str(cars) + "cars")
+# to ASK THE COMPUTER HOW OLD ARE YOU
+age = input("how old are you")
+print(age + "? My daughter is older than you")
+
+
+# make a list with atleast 5 colors
+colors =["green","purple","pink","blue","red","orange"]
+colors.append("cyan")
+
+print(colors)
+
+colors.pop(1)
+print(colors)
