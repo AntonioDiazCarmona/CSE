f75ada0475382162886efa41517d4b3fe018d8de
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2019-01-14 08:42:18 -0800

    progress

--- notes/Antonio Diaz-Hang Man.py
+++ notes/Antonio Diaz-Hang Man.py
@@ -2,15 +2,36 @@ import random
 words =["tomatoes","foodmax","fortnite","chicken","elephant","noodle","pepperoni","computer","pycharm","triangle",]
 the_word = random.choice(words)
 print(the_word)
+letters =["a","b","c","d","e","f","g","h","i","j","k","l","m","o","p""q","r","s","t","u","v","w","x","y","z"]
+letter = input("")
+if letter in the_word:
+    print(letter)
+
+for character in list1:
+    if character == "u":
+        # replace with a *
+        currrent_index = list1.index(character)
+        list1.pop(currrent_index)
+        list1.insert(currrent_index, "*")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # string1 = "turquoise"
 # list1 = list(string1)
 # print(list1)
-#
-#
-#
 
-#
 # for character in the_word:
 #     if character == "u":
 #         # replace with a *
--- notes/HangMan pre code.py
+++ notes/HangMan pre code.py
@@ -4,25 +4,26 @@ print("Start guessing...")
 import random
 words =["tomatoes","foodmax","fortnite","chicken","elephant","noodle","pepperoni","computer","pycharm","triangle",]
 the_word = random.choice(words)
-string1 = (the_word)
-list1 = list(string1)
-print(list1)
-
 print(the_word)
-guesses = 10
+letters =["a","b","c","d","e","f","g","h","i","j","k","l","m","o","p""q","r","s","t","u","v","w","x","y","z"]
+letter = input("")
+if letter in the_word:
+    print(letter)
+
+turns_left = 10
 turns = 10
 while turns > 0:
     failed = 0
-    for letter in the_word:
-        if letter in guesses:
-            print(letter)
+    letter = input("")
+    if letter in the_word:
+        print(letter)
         else:
             print("_")
             failed += 1
     if failed == 0:
         print("You Won")
 guess = input("guess a letter")
-guesses += guess
+turns_left += guess
 if guess not in the_word:
     turns -= 1
     print("Wrong")
