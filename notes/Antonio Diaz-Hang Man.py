e2481761bee7ba56cbd270e66bb81103d40a728a
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2019-01-22 08:42:04 -0800

    ALot of progress today

--- notes/Antonio Diaz-Hang Man.py
+++ notes/Antonio Diaz-Hang Man.py
@@ -20,14 +20,19 @@ for character in list1:
 
 
 
-the_word=["_"]
-item_to_replace =("_")
-replacement_value =[]
- indices_to_replace = [i for i,x in enumerate(a) if x==item_to_replace]
-indices_to_replace
-[0, 5, 10]
-for i in indices_to_replace:
-  the_word[i] = replacement_value
+
+
+
+output=['_']
+for i in range(for len(the_word))
+['_']
+output=letters
+output['_','_','_','_','_']
+letters_used.append
+
+
+
+
 
 
 
@@ -45,13 +50,14 @@ for i in indices_to_replace:
 
 
 
-# string1 = "turquoise"
-# list1 = list(string1)
-# print(list1)
+string1 = the_word
+list1 = list(string1)
+print(list1)
 
-# for character in the_word:
-#     if character == "u":
-#         # replace with a *
-#         currrent_index = the_word.index(character)
-#         the_word.pop(currrent_index)
-#         the_word.insert(currrent_index, "*")
+for character in list1:
+    if character == letters:
+        # replace with a *
+        currrent_index = list1.index(character)
+        list1.pop(currrent_index)
+        list1.insert(currrent_index, "*")
+
--- notes/HangMan pre code.py
+++ notes/HangMan pre code.py
@@ -2,28 +2,42 @@ import random
 import string
 words =["tomatoes","foodmax","fortnite","chicken","elephant","noodle","pepperoni","computer","pycharm","triangle",]
 the_word = random.choice(words)
-print(the_word)
+# print(the_word)
 letters = list(string.ascii_letters)
 turns_left = 10
 failed = 0
 letters_used = []
+hidden_word = []
+list_of_letters_in_word = list(the_word)
+print(list_of_letters_in_word)
+
+for character in list_of_letters_in_word:
+    if character in letters:
+        # replace with a *
+        hidden_word.append("*")
 
 while turns_left > 0:
     # Create the output
-
+    print("".join(hidden_word))
 
     # Taking a guess
     letter = input("")
     letters_used.append(letter)
+
     if letter in the_word:
         print("I found one!")
-        print(letters_used)
-        print(letter)
+        for i in range(len(the_word)):
+            if the_word[i] == letter:
+                hidden_word.pop(i)
+                hidden_word.insert(i, letter)
+
     else:
         turns_left -= 1
         print("Wrong")
         print("You have", + turns_left, 'more guesses')
-        print(letters_used)
-        print(letter)
+    print(letters_used)
+    print(letter)
     if turns_left == 0:
         print("You Loose better luck next time")
+    if all letter in (i)the_word
+        print("YOU WON")
