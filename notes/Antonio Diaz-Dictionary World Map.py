8fbc9a41e665adfb389e6fef9552ead907a570df
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2019-01-31 08:44:49 -0800

    starting code

--- notes/Antonio Diaz-Dictionary World Map.py
+++ notes/Antonio Diaz-Dictionary World Map.py
@@ -1,18 +1,37 @@
 world_map = {
     'R19A':{
-        'NAME': "Wiebe's classroom",
-        'DESCRIPTION': "This is the classroom that you are in right now."
-                       "It has two exits to the north side",
+        'NAME': "Abandoned  Mansion",
+        'DESCRIPTION': "What your looking at is an abandoned mansion from the gate door a mile away"
+                       "This mansion who no one know's who built it or who live here and some people have even said that"
+                       "The Mansion has been abandoned for centuries and it has stayed stay the same."
+                       "The mansion has 9 rooms and 9 door or does it? it also has 9 statues which are flip upside down",
         'PATHS': {
-        "NORTH": "PARKING_LOT"
+        "NORTH": "MANSION_DOOR"
         }
-    }
-    'PARKING_LOT':{
-        'NAME' : "The Edison parking lot",
-        'DESCRIPTION':"There are cars parked here.to"
+    },
+    'MANSION_DOOR':{
+        'NAME' : "The ABANDONED MANSION DOOR",
+        'DESCRIPTION':"There are plants here too"
         "the south is MR.Wiebe's room",
         'paths': {
             'SOUTH': "R19A"
         }
     }
 }
+#other variables
+current_node = world_map['R19A']
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
+playing = True
+# controller
+while playing:
+    command = input(">_")
+    if command.lower() in ('q', 'quit', 'exit'):
+        playing = False
+    elif command.upper() in directions:
+         try:
+            room_name = current_node["PATHS"][command.upper()]
+            current_node = world_map[room_name]
+         except KeyError:
+             print("I can't go that way.")
+         else:
+             print("command Not Recognized")
