5b09a57949e523ed6b3bd1503a929ec731a3adfe
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2019-02-05 08:44:38 -0800

    2 new places

--- notes/Antonio Diaz-Dictionary World Map.py
+++ notes/Antonio Diaz-Dictionary World Map.py
@@ -33,16 +33,32 @@ world_map = {
         'NAME': "The ABANDONED MANSION DOOR",
         'DESCRIPTION':"There are plants here and "
         " to the south is a window where you can look through",
-        'paths': {
+        'PATHS': {
             'EAST': "Mansion_Window",
             'WEST': "STATUE"
         }
     },
     "STATUE": {
-        'NAME': "Coble stone statue "
-        'DESCRIPTION':
+        'NAME': "Cobblestone statue ",
+        'DESCRIPTION':"What your looking at is a coble stone statue of a man with no head "
+                      "You can see a metal piece on top of the statue with there is no head"
+                      "the statue is to high you will need something to climb on the statue and get the metal piece",
+        'PATHS':{
+            'SOUTH': "SIDE OF MANSION"
+        }
+    },
+    "SIDE OF MANSION": {
+            'NAME': "South side of mansion",
+        "DESCRIPTION": "You are walking throught the south side of mansion which leads to the backyard",
+        'PATHS':{
+            'DOWN':"you are going down the side of mansion",
+           'EAST':"Back yard of mansion"
+        }
     }
-}
+},
+
+
+
 #other variables
 current_node = world_map['MANSION_DOOR']
 directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
@@ -59,4 +75,4 @@ while playing:
          except KeyError:
              print("I can't go that way.")
          else:
-             print("command Not Recognized")
+             print("Command Not Recognized")
