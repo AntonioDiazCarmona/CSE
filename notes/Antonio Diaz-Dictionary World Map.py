548b1c02308e804b78c66bd58c7082aac8a04272
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2019-02-06 08:43:03 -0800

    ALOT OF PROGRESS TODAY

--- notes/Antonio Diaz-Dictionary World Map.py
+++ notes/Antonio Diaz-Dictionary World Map.py
@@ -31,11 +31,18 @@ world_map = {
     },
     'MANSION_DOOR':{
         'NAME': "The ABANDONED MANSION DOOR",
-        'DESCRIPTION':"There are plants here and "
+        'DESCRIPTION':"The door seems to be closed and around are plants and "
         " to the south is a window where you can look through",
         'PATHS': {
-            'EAST': "Mansion_Window",
+            'EAST': "MANSION_WINDOW",
             'WEST': "STATUE"
+        }
+    },
+    'MANSION_WINDOW': {
+        'NAME': "EAST SIDE WINDOW",
+        'DESCRIPTION': "The window seems to be a mirror ",
+        'PATHS': {
+            'WEST': "STATUE"                                        # YOU CAN KEEP GOING HERE
         }
     },
     "STATUE": {
@@ -49,15 +56,57 @@ world_map = {
     },
     "SIDE OF MANSION": {
             'NAME': "South side of mansion",
-        "DESCRIPTION": "You are walking throught the south side of mansion which leads to the backyard",
+        "DESCRIPTION": "You are looking at the south side of mansion which leads to the backyard",
         'PATHS':{
-            'DOWN':"you are going down the side of mansion",
-           'EAST':"Back yard of mansion"
+            'SOUTH':"you are going down the side of mansion",
+        }
+    },
+    "DOWN SIDE OF MANSION": {
+            'NAME': "Down south side of mansion",
+        "DESCRIPTION": "You are walking down the south side of mansion",
+        'PATHS':{
+                'EAST':"You are at the backyard"
+        }
+    },
+    "BACKYARD OF MANSION": {
+            'NAME': "EAST BACKYARD",
+        "DESCRIPTION": "You are at the back yard and their seems to be alot of items at the back like stools,slide,"
+                       "and a small little tree",
+        'PATHS':{      # HERE WE HAVE TO PUT WHERE WE PICK UP ITEMS
+            'WEST':"Side of Mansion"
+        }
+    },
+    "SIDE OF MANSION": {
+            'NAME':" WEST SIDE OF MANSION",
+        "DESCRIPTION":" You are looking at the side of the mansion you came from",
+        'PATHS':{
+            'NORTH': "UP SIDE OF MANSION"
+        }
+    },
+    "UP SIDE OF MANSION": {
+        'NAME': "UP NORTH SIDE OF MANSION",
+        'DESCRIPTION': "This is the way you came from in the begining to go to the back yard",
+        'PATHS': {
+            'EAST':"SIDE OF MANSION"
+        }
+    },
+    "SIDE OF MANSION": {
+        'NAME': "SIDE OF MANSION THAT lEAD TO THE SIDE OF MANSION",
+        'DESCRIPTION': "You are at the west side of mansion to the east you can see the coble stone statue from before",
+        'PATHS': {
+            'EAST':"COBBLE STONE STATUE"
+        }
+    },
+    "COBBLE STONE STATUE": {
+        'NAME': "WEST COBBLE STONE STATUE",
+        # where you place the stool to look on top of the statue with no head
+        'DESCRIPTION': "You are at the cobble stone statue now try to see what is the top of the coblestone statue with no head"
+                "by placing the stool in climbing it",
+        'PATHS': {
+            'EAST':
         }
     }
-},
-
-
+}
 
 #other variables
 current_node = world_map['MANSION_DOOR']
