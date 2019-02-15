c1b51e9fd941dc39f27903f2ac03697a880657f6
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2019-02-15 08:43:23 -0800

    Yea i dont understand nothing

--- notes/Object.py
+++ notes/Object.py
@@ -1,17 +1,16 @@
 class Faucet(object):
-    def __init__(self,pull_handles,water_per_minute,amount_water,hot,cold):
+    def __init__(self, pressure_cold = (1,100),pressure_hot =(1,100),gallons = 50,water_per_minute = 1.5*(1,10)):
         #these are things that a WaterGun has.
         # all of these should be relevant to our program.
-        self.cold          # hot and cold is a methiod
-        self.hot
-        self.pressure               # method
-        self.amount_water
-        self.water_per_minute =
-        self.pull_handles
-    def pull_handles(self, time):
-        if self.handles:
+        self.percent_cold =  pressure_cold         # hot and cold is a method
+        self.percent_hot = pressure_hot
+        self.pressure = water_per_minute     # method
+        self.amount_water = gallons
+        self.pull_handles = True
+    def turn_on(self,time):
+        if self.pull_handles:
             if self.pressure <= 0:
-                print("There's no pressure!")
+                print("There's no water coming out!")
             elif self.water_per_minute < time:
                print("You used % water per %s" % self.pressure)
                self.pressure = 0
@@ -23,7 +22,7 @@ class Faucet(object):
 
     def pump_it_up(self):
         self.pressure = 5
-        print("You pump the tank back to full pressure")
+        print("water is coming out the faucet at  ")
 
 # Initialize the objects
-Faucet.
+# Faucet.Pressure
