1fb8f690a37e77c53b4097afdffe8c3264169503
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2019-02-26 08:43:43 -0800

    just making the controller

--- notes/Antonio-Diaz-World Map OOP.py
+++ notes/Antonio-Diaz-World Map OOP.py
@@ -1,5 +1,5 @@
 class Room(object):
-    def __init__(self, name, description ="", north=None ,south=None, east=None,west = None, up = None, down = None):
+    def __init__(self, name, description="", north=None, south=None, east=None, west=None, up=None, down=None):
         self.name = name
         self.north = north
         self.south = south
@@ -10,34 +10,54 @@ class Room(object):
         self.down = down
 
 
+class Player(object):
+    def __init__(self, starting_location):
+        self.health = 100
+        self.current_location = starting_location
+        self.inventory = []
+        self.damage = 10
+
+    def move(self, new_location):
+        """this method moves charecters to a new location
+
+        :param newlocation:  the variable containing a room object
+        """
+        self.current_location = new_location
+
+
+
+
+
+
 ABANDONED_MANSION = Room("Abandoned Mansion", "What your looking at is an abandoned mansion from the gate door a mile "
                                               "away"
                                               "This mansion who no one know's who built it or who live here and some"
                                               " people have even said that the mansion has been abandoned for centuries"
                                               " and it has stayed the same. THe mansion has 15 rooms or does it?"
                                               " It also has a random statue with no head")
+player = Player(ABANDONED_MANSION)
 
 MANSION_DOOR = Room("The ABANDONED MANSION DOOR", "The door seems to be closed and around you can see plants."
                                                   " To the south there is a window. To the west is a statue that"
                                                   " has no head.The door seems to be closed and around you can see"
                                                   " plants.")
 
-MANSION_WINDOW = Room("EAST SIDE WINDOW","The window seems to be a mirror ")
+MANSION_WINDOW = Room("EAST SIDE WINDOW", "The window seems to be a mirror ")
 
-EAST_SIDE_OF_MANSION = Room("EAST SIDE OF MANSION","The east side of the mansion seems to be blocked off by a giant"
-                                                     " tree.")
-V2MANSION_WINDOW = Room("WEST SIDE WINDOW OF MANSION","You are back to the window that seems to be a mirror")
+EAST_SIDE_OF_MANSION = Room("EAST SIDE OF MANSION", "The east side of the mansion seems to be blocked off by a giant"
+                                                     "tree.")
+V2MANSION_WINDOW = Room("WEST SIDE WINDOW OF MANSION", "You are back to the window that seems to be a mirror")
 
-STATUE = Room("Cobblestone statue","What your looking at is a coble stone statue of a man with no head "
+STATUE = Room("Cobblestone statue", "What your looking at is a coble stone statue of a man with no head "
                       "You can see a metal piece on top of the statue with there is no head"
                       "the statue is to high you will need something to climb on the statue and get the metal piece")
 
-V1_SIDE_OF_MANSION = Room("south side of mansion","You are looking at the south side of mansion which leads"
-                                                  " to the backyard")
+V1_SIDE_OF_MANSION = Room("south side of mansion", "You are looking at the south side of mansion which leads"
+                                                  "to the backyard")
 
-DOWN_SIDE_OF_MANSION = Room("Down south side of mansion","You are walking down the south side of mansion")
+DOWN_SIDE_OF_MANSION = Room("Down south side of mansion", "You are walking down the south side of mansion")
 
-BACKYARD_OF_MANSION = Room("EAST BACKYARD","You are at the back yard and their seems to be alot of items at"
+BACKYARD_OF_MANSION = Room("EAST BACKYARD", "You are at the back yard and their seems to be alot of items at"
                                            " the backyard like stools,slide,and a small little tree")     # here you would get a stool
 
 V2_SIDE_OF_MANSION = Room("WEST SIDE OF MANSION","You are looking at the side of the mansion you came from")
@@ -50,7 +70,7 @@ V3_SIDE_OF_MANSION = Room("West side of mansion","You are at the west side of ma
 
 COBBLE_STONE_STATUE = Room("WEST COBBLE STONE STATUE", "You are at the cobble stone statue now try to see what"
                                                        " is the top of the cobblestone statue with no head"
-                                                       "by placing the stool that you collected from the"   # you find the key...
+                                                       "by placing the stool that you collected from the"# you find the key...
                                                        " backyard and try climbing it to see what was"
                                                        " at the top of the no head statue")
 
@@ -60,7 +80,8 @@ V2MANSION_DOOR = Room("FRONT MANSION DOOR", "Your are back again to front mansio
 
 INSIDE_MANSION = Room("INSIDE OF MANSION", "Their seems to be a lot of rooms here.Their are two stair leading up to "
                                            "the second floor which where you can see more rooms.Their is also a"
-                                           " random doll sitting on the stairs")  # you open the door with the key,get in
+                                           " random doll sitting on the stairs")
+# you open the door with the key,get in.
 
 ABANDONED_MANSION.south = MANSION_DOOR
 MANSION_DOOR.east = MANSION_WINDOW
@@ -86,3 +107,22 @@ V2MANSION_DOOR.south = INSIDE_MANSION
 # #put them in quotes
 # R19A = Room("R19A", 'Parking_lot')
 # parking_lot = Room ("The parking lot",None,R19A)
+playing = True
+directions = ["north", "south", "east", "west", "up", "down"]
+
+# controller
+while playing:
+    print(player.current_location.name)
+    print(player.current_location.description)
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command.lower() in directions:
+        try:
+            #command ='north'       #if their is now location you put none                           # so this basically tell it if i has a path to north
+            room_object_that_we_move_to = getattr(player.current_location,command)
+            player.move(room_object_that_we_move_to)
+        except KeyError:
+            print("I can't go that way.")
+    else:
+        print("Command Not Recognized")
