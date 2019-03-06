b8868a976fad6e58c1d8709d8f5aea70bdb3600b
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2019-03-06 13:36:40 -0800

    item

--- notes/Antonio-Diaz-Items.py
+++ notes/Antonio-Diaz-Items.py
@@ -1,28 +1,52 @@
-class Items(object):
+class Item(object):
     def __init__(self, name, weight):
         self.name = name
         self.weight = weight
 
-class Axe(Items):
-    def __init__(self, name, types, blade, damage):
-        super(Axe, self).__init__(name, types)
+
+class Axe(Item):
+    def __init__(self, name, weight, blade, damage):
+        super(Axe, self).__init__(name, weight)
         self.handle = True
         self.blade = blade
         self.damage = damage
 
+    def Axe(self, long):
+        self.blade = long
 
-    def Axe(self, short):    # fix this
-        self.blade = short
+    def damage(self):
+        self.damage = 10
+
+
+class LongAxe(Axe):
+    def __init__(self, name, weight):
+          super(LongAxe, self).__init__(name, weight, "long", 10)
+
+
+class Sword(Item):
+    class ShortSword(Item):
+        def __init__(self, name, weight, blade, damage):
+            super(Sword, self).__init__(name, weight)
+            self.handle = True
+            self.blade = blade
+            self.damage = damage
+
+        def ShortSword(self, short):
+            self.blade = short
 
         def damage(self):
             self.damage = 10
 
-
-  class LongAxe(Axe):
-      def __init__(self):
-          super(LongAxe,self).__init__(name, types)
-          self.collectable
-          my_Axe = LongAxe
+    class ShortSword(Sword):
+        def __init__(self, name, weight):
+            super(ShortSword, self).__init__(name, weight, "Short", 10)
 
 
 
+
+
+
+
+
+    my_axe = LongAxe("Bertha", 15)
+    My_sword = ShortSword("idk",5)
