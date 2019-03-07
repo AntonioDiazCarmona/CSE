861fd530af29c3e9731996ea8ad2d50cd77c5f7e
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2019-03-07 08:44:00 -0800

    Trying to fix items

--- notes/Antonio-Diaz-Items.py
+++ notes/Antonio-Diaz-Items.py
@@ -1,3 +1,5 @@
+import random
+
 class Item(object):
     def __init__(self, name, weight):
         self.name = name
@@ -21,32 +23,49 @@ class Axe(Item):
 class LongAxe(Axe):
     def __init__(self, name, weight):
           super(LongAxe, self).__init__(name, weight, "long", 10)
-
+my_axe = LongAxe
 
 class Sword(Item):
-    class ShortSword(Item):
-        def __init__(self, name, weight, blade, damage):
-            super(Sword, self).__init__(name, weight)
-            self.handle = True
-            self.blade = blade
-            self.damage = damage
+    def __init__(self, name, weight, blade, damage):
+        super(Sword, self).__init__(name, weight)
+        self.handle = True
+        self.blade = blade
+        self.damage = damage
 
-        def ShortSword(self, short):
-            self.blade = short
+    def blade(self, short):
+        self.blade = short
 
-        def damage(self):
-            self.damage = 10
-
-    class ShortSword(Sword):
-        def __init__(self, name, weight):
-            super(ShortSword, self).__init__(name, weight, "Short", 10)
+    def damage(self):
+        self.damage = 10
 
 
+class ShortSword(Sword):
+    def __init__(self):
+        super(ShortSword, self).__init__("short sword", 15, "short", 15)
+
+    def attack(self):
+        random.randint(12, 15)
 
 
+my_shortsword = ShortSword
 
+class Bow(Item):
+     def __init__(self, name, weight, range, shoot, damage, arrows = 10):
+         super(Bow, self).__init__(name, weight)
+         self.range = range
+         self.shoot = shoot
+         self.damage = damage
+         self.arrows = arrows
 
+     def Crossbow(self):
+         self.Crossbow = shoot
+        if
+            self.shoot = True
+            self.arrows =-1
+     def damage = 50
+     def range = 20
+shoot = True
 
+class CrossBow(Bow):
 
-    my_axe = LongAxe("Bertha", 15)
-    My_sword = ShortSword("idk",5)
+     def __init__(self, name, weight,"Normal",15
