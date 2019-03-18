053bd02ff42f325ae16ed4c84340f17823c6bda8
AntonioDiazCarmona <42383456+AntonioDiazCarmona@users.noreply.github.com>
2019-03-18 08:43:28 -0700

    Combining items in oop

--- notes/Antonio-Diaz-World Map OOP.py
+++ notes/Antonio-Diaz-World Map OOP.py
@@ -1,3 +1,10 @@
+debug = True
+if debug:
+    delay = 0.01
+else:
+    delay = 1
+
+
 class Room(object):
     def __init__(self, name, description="", north=None, south=None, east=None, west=None, up=None, down=None):
         self.name = name
@@ -9,16 +16,409 @@ class Room(object):
         self.up = up
         self.down = down
 
-class Item(object):
-    def __init__(self, name):
-        self.name = name
 
-class weapon(Item):
-    def __init__(self, name, damage):
-        super(weapon, self).__init__(name)
+class Item(object):
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+
+class Axe(Item):
+    def __init__(self, name, weight, blade, damage, durability):
+        super(Axe, self).__init__(name, weight)
+        self.handle = True
+        self.blade = blade
+        self.damage = damage
+        self.durability = durability
+
+    def blade(self, long):
+        self.blade = long
+
+    def damage(self):
+        self.damage = 10
+
+
+class LongAxe(Axe):
+    def __init__(self, name, damage, blade, durability, weight):
+        super(LongAxe, self).__init__(name, weight, blade, damage, durability)
+
+    def attack(self):
+        if self.durability > 0:
+            print("you did a heavy swing and did", self.damage, "damage")
+            self.durability = self.durability - 5
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you still have", self.durability, "durability left on your long axe")
+        else:
+            print("you died")
+
+
+class ShortAxe(Axe):
+    def __init__(self, name, damage, blade, durability, weight):
+        super(ShortAxe, self).__init__(name, weight, blade, damage, durability)
+
+    def attack(self):
+        if self.durability > 0:
+            print("you did swing and did", self.damage, "damage")
+            self.durability = self.durability - 5
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you still have", self.durability, "durability left on your short axe")
+        else:
+            print("you died")
+
+
+class Sword(Item):
+    def __init__(self, name, weight, blade, damage, durability):
+        super(Sword, self).__init__(name, weight)
+        self.handle = True
+        self.blade = blade
+        self.damage = damage
+        self.durability = durability
+
+    def blade(self, long):
+        self.blade = long
+
+    def damage(self):
+        self.damage = 10
+
+
+class ShortSword(Sword):
+    def __init__(self, name, damage, blade, durability, weight):
+        super(ShortSword, self).__init__(name, weight, blade, damage, durability)
+
+    def attack(self):
+        if self.durability > 0:
+            print("you did swing your short sword and did", self.damage, "damage")
+            self.durability = self.durability - 5
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you still have", self.durability, "durability left on your short sword")
+        else:
+            print("you died")
+
+
+class LongSword(Sword):
+    def __init__(self, name, damage, blade, durability, weight):
+        super(LongSword, self).__init__(name, weight, blade, damage, durability)
+
+    def attack(self):
+        if self.durability > 0:
+            print("you did swing your long sword and did", self.damage, "damage")
+            self.durability = self.durability - 5
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you still have", self.durability, "durability left on your long sword")
+        else:
+            print("you died")
+
+
+class Bow(Item):
+    def __init__(self, name, weight, arrows, damage, durability):
+        super(Bow, self).__init__(name, weight)
+        self.arrows = arrows
+        self.damage = damage
+        self.durability = durability
+
+    def arrows(self, long):
+        self.arrows = long
+
+    def damage(self):
+        self.damage = 10
+
+
+class LongBow(Bow):
+    def __init__(self, name, weight, arrows, damage, durability):
+        super(LongBow, self).__init__(name, weight, arrows, damage, durability)
+
+    def shoot(self):
+        if self.arrows > 0:
+            print('swishh swishh I just shot an arrow, you got hit and received', self.damage, 'damage')
+            self.arrows = self.arrows - 1
+            self.durability = self.durability - 1
+        else:
+            print('...')
+
+    def how_many_arrows_left(self):
+        if self.arrows > 0:
+            print("You STILL have", self.arrows, "arrows left")
+        else:
+            print("You got nothing")
+            print(self.arrows)
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you still have", self.durability, "durability left on your long bow")
+        else:
+            print("you have no durability")
+
+
+class ShortBow(Bow):
+    def __init__(self, name, weight, arrows, damage, durability):
+        super(ShortBow, self).__init__(name, weight, arrows, damage, durability)
+
+    def shoot(self):
+        if self.arrows > 0:
+            print('swishh swishh I just shot an arrow, you got hit and received', self.damage, 'damage')
+            self.arrows = self.arrows - 1
+            self.durability = self.durability - 1
+        else:
+            print('...')
+
+    def how_many_arrows_left(self):
+        if self.arrows > 0:
+            print("You STILL have", self.arrows, "arrows left")
+        else:
+            print("You got nothing")
+            print(self.arrows)
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you still have", self.durability, "durability left on your long bow")
+        else:
+            print("you have no durability")
+
+
+class CrossBow(Bow):
+    def __init__(self, name, weight, arrows, damage, durability):
+        super().__init__(name, weight, arrows, damage, durability)
+
+    def shoot(self):
+        if self.arrows > 0:
+            print('swishh swishh I just shot an arrow, you got hit and received', self.damage, 'damage')
+            less_arrows = int(input("how many arrows would you like to shoot? "))
+            self.arrows = self.arrows - less_arrows
+            self.durability = self.durability - less_arrows
+        else:
+            print('...')
+
+    def how_many_arrows_left(self):
+        if self.arrows > 0:
+            print("You STILL have", self.arrows, "arrows left")
+        else:
+            print("You got nothing")
+            print(self.arrows)
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you still have", self.durability, "durability left on your long bow")
+        else:
+            print("you have no durability")
+
+
+class Staff(Item):
+    def __init__(self, name, weight, crystal, magic_damage, durability):
+        super(Staff, self).__init__(name, weight)
+        self.handle = True
+        self.crystal = crystal
+        self.MagicDamage = magic_damage
+        self.durability = durability
+
+
+class FireStaff(Staff):
+    def __init__(self, name, weight, crystal, magic_damage, durability):
+        super(FireStaff, self).__init__(name, weight, crystal, magic_damage, durability)
+
+    def shoot_fire_ball(self):
+        if self.durability > 0:
+            print("you shot a fire ball and did ", self.MagicDamage,
+                  "damage, The thing is also taking burn damage of 1 health per sec for 5 secs")
+            self.durability = self.durability - 2
+            import time
+            number_of_seconds = 5
+            for _ in range(number_of_seconds):
+                time.sleep(1 * delay)
+                print("You are taking 1 damage do to fire damage from the fire ball")
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you still have", self.durability, "durability left on your Fire Staff")
+        else:
+            print("you died")
+
+
+class IceStaff(Staff):
+    def __init__(self, name, weight, crystal, magic_damage, durability):
+        super(IceStaff, self).__init__(name, weight, crystal, magic_damage, durability)
+
+    def freeze(self):
+        if self.durability > 0:
+            print("you shot a Ice ball and ", self.MagicDamage, "damage,You also froze the thing for 10 seconds")
+            self.durability = self.durability - 2
+            import time
+            number_of_seconds = 10
+            for _ in range(number_of_seconds):
+                time.sleep(1 * delay)
+                print("You are frozen ")
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you still have", self.durability, "durability left on your Ice Staff ")
+        else:
+            print("you died")
+
+
+class LightingStaff(Staff):
+    def __init__(self, name, weight, crystal, magic_damage, durability):
+        super(LightingStaff, self).__init__(name, weight, crystal, magic_damage, durability)
+
+    def attack(self):
+        if self.durability > 0:
+            print("You shot a lighting ball and did ", self.MagicDamage, "damage")
+            self.durability = self.durability - 1
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you still have", self.durability, "durability left on your lighting Staff")
+        else:
+            print("you died")
+
+
+class Shield(Item):
+    def __init__(self, name, weight, blockdamage, durability):
+        super(Shield, self).__init__(name, weight)
+        self.blockdamage = blockdamage
+        self.durability = durability
+
+
+class ArmShield(Shield):
+    def __init__(self, name, weight, blockdamage, durability):
+        super(ArmShield, self).__init__(name, weight, blockdamage, durability)
+
+    def block(self):
+        if self.durability > 0:
+            damage = int(input("how much damage would you like to recieve? "))
+            self.shield_broke(damage)
+            self.durability = self.durability - damage
+            print("you blocked", damage, "damage")
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you still have", self.durability, "durability left on your arm shield")
+        else:
+            print("you ran out of durability and your shield broke")
+
+    def shield_broke(self, damage):
+        if damage > self.blockdamage:
+            print("Your shield broke by taking", self.blockdamage - damage,
+                  "damage over the block damage of what the arm shield handle at a time")
+
+
+class MythicalShield(Shield):
+    def __init__(self, name, weight, blockdamage, durability):
+        super(MythicalShield, self).__init__(name, weight, blockdamage, durability)
+
+    def block(self):
+        if self.durability > 0:
+            damage = int(input("how much damage would you like to recieve on your mythical shield? "))
+            self.durability = self.durability + damage  # changes
+            print("You got attack and you mythical shield absorve", damage,
+                  "damage and turned it into durabibility so your durability increase")
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you have ", self.durability, "durability after absorbing the damage you took  ")
+
+
+class NerfGun(Item):
+    def __init__(self, name, weight, bullets, damage, durability):
+        super(NerfGun, self).__init__(name, weight)
+        self.bullets = bullets
+        self.damage = damage
+        self.durability = durability
+
+
+class AssaultRifle(NerfGun):
+    def __init__(self, name, weight, bullets, damage, durability):
+        super(AssaultRifle, self).__init__(name, weight, bullets, damage, durability)
+
+    def shoot(self):
+        if self.bullets > 0:
+            print('You shot a nerf bullet  with your assault rifle and did ', self.damage, 'damage')
+            self.bullets = self.bullets - 1
+            self.durability = self.durability - 1
+        else:
+            print('...')
+
+    def how_many_bullets_left(self):
+        if self.bullets > 0:
+            print("You STILL have", self.bullets, "bullets left")
+        else:
+            print("You got nothing")
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you still have", self.durability, "durability left on your nerf assault rifle")
+        else:
+            print("you have no durability")
+
+
+class BurstRifle(NerfGun):
+    def __init__(self, name, weight, bullets, damage, durability):
+        super(BurstRifle, self).__init__(name, weight, bullets, damage, durability)
+
+    def shoot(self):
+        if self.bullets > 0:
+            print('You shot 3 nerf bullets with your burst rifle and did ', self.damage, 'damage')
+            self.bullets = self.bullets - 3
+            self.durability = self.durability - 1
+        else:
+            print('...')
+
+    def how_many_bullets_left(self):
+        if self.bullets > 0:
+            print("You STILL have", self.bullets, "bullets left")
+        else:
+            print("You got nothing")
+
+    def amount_of_durability(self):
+        if self.durability > 0:
+            print("you still have", self.durability, "durability left on your nerf burst rifle")
+        else:
+            print("you have no durability")
+
+
+class ThrowingThings(Item):
+    def __init__(self, name, weight, damage):
+        super(ThrowingThings, self).__init__(name, weight)
         self.damage = damage
 
-class charecter(object):
+
+class Rock(ThrowingThings):
+    def __init__(self, name, weight, damage):
+        super(Rock, self).__init__(name, weight, damage)
+
+    def throw(self):
+        if self.damage > 0:
+            print("you threw a rock and did", self.damage, "damage")
+        else:
+            print("You have no muscle and didn't throw the rock.")
+
+
+class NinjaStar(ThrowingThings):
+    def __init__(self, name, weight, damage):
+        super(NinjaStar, self).__init__(name, weight, damage)
+
+    def throw(self):
+        if self.damage > 0:
+            print("you threw a ninja star and did", self.damage, "damage")
+        else:
+            print("you threw the ninja star but missed...")
+
+
+
+
+
+
+
+#  Charecters
+
+
+class Character(object):
     def __init__(self, name, health, weapon, armor):
         self.name = name
         self.health = health
@@ -51,25 +451,28 @@ class Player(object):
         self.current_location = new_location
 
 
-ABANDONED_MANSION = Room("Abandoned Mansion", "What your looking at is an abandoned mansion from"
-                                              " the gate door a mile away"
-                                              "This mansion who no one know's who built it or who live here and some"
-                                              " people have even said that the mansion has been abandoned for centuries"
-                                              " and it has stayed the same. THe mansion has 15 rooms or does it?"
-                                              " It also has a random statue with no head")
+Long_Axe = LongAxe("LongAxe", 30, "Long", 100, 20)
+Short_Axe = ShortAxe("ShortAxe", 15, "small", 50, 10)
+Short_Sword = ShortSword("ShortSword", 5, "small", 50, 5)
+Long_Sword = LongSword("ShortSword", 5, "small", 50, 5)
+Long_Bow = LongBow("Long Bow", 15, 10, 50, 500)
+Short_Bow = ShortBow("Long Bow", 10, 30, 30, 300)
+Cross_Bow = CrossBow("Long Bow", 10, 30, 30, 1000)
+Fire_Staff = FireStaff("FireStaff", 20, "Long", 60, 20)
+Ice_staff = IceStaff("IceStaff", 20, "Long", 50, 35)
+lighting_staff = LightingStaff("lighting staff", 15, "long", 100, 25)
+Arm_shield = ArmShield("Arm Shield", 10, 50, 1000)
+Mythical_Shield = MythicalShield("Mythical Shield", 10, 0, 500)
+Assault_Rifle = AssaultRifle("Assault Rifle", 15, 30, 20, 200)
+Burst_Rifle = BurstRifle("Burst Rifle", 15, 20, 15, 100)
+rock = Rock("Rock", 4, 10)
+Ninja_Star = NinjaStar("Ninja Star", 2, 15)
 
-sword = weapon("sword", 15)
-sword2 = weapon("orc sword", 5)
 
 
-player = Player(ABANDONED_MANSION)
 
-
-c1 = charecter("Orc1", 100, sword, None)
-c2 = charecter("Orc2",100, sword2, None)
-
-c1.attack(c2)
-c2.attack(c1)
+c1 = Character("Orc1", 100, Long_Axe, None)
+c2 = Character("Orc2", 100, Long_Bow, None)
 
 
 MANSION_DOOR = Room("The ABANDONED MANSION DOOR", "The door seems to be closed and around you can see plants."
@@ -118,6 +521,16 @@ V2MANSION_DOOR = Room("FRONT MANSION DOOR", "Your are back again to front mansio
 INSIDE_MANSION = Room("INSIDE OF MANSION", "Their seems to be a lot of rooms here.Their are two stair leading up to "
                                            "the second floor which where you can see more rooms.Their is also a"
                                            " random doll sitting on the stairs")
+
+ABANDONED_MANSION = Room("Abandoned Mansion", "What your looking at is an abandoned mansion from"
+                                              " the gate door a mile away"
+                                              "This mansion who no one know's who built it or who live here and some"
+                                              " people have even said that the mansion has been abandoned for centuries"
+                                              " and it has stayed the same. THe mansion has 15 rooms or does it?"
+                                              " It also has a random statue with no head")
+
+
+
 # you open the door with the key,get in.
 
 ABANDONED_MANSION.south = MANSION_DOOR
@@ -152,15 +565,9 @@ COBBLE_STONE_STATUE.east = V2MANSION_DOOR
 V2MANSION_DOOR.west = COBBLE_STONE_STATUE
 V2MANSION_DOOR.south = INSIDE_MANSION
 INSIDE_MANSION.north = V2MANSION_DOOR
-# #option 1
-# # add the dependent rooms after
-# R19A = Room("R19A")
-# parking_lot = Room('The parking lot',None,R19A)
-# R19A.north = parking_lot
-# #option 2
-# #put them in quotes
-# R19A = Room("R19A", 'Parking_lot')
-# parking_lot = Room ("The parking lot",None,R19A)
+
+player = Player(ABANDONED_MANSION)
+
 playing = True
 directions = ["north", "south", "east", "west", "up", "down"]
 
